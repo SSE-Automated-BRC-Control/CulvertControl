e38ab68b658f4451ec0bd4563916e96e1ba566aa
ArloReichert <reichertarlo@gmail.com>
2021-11-19 00:06:04 -0600

    added main and simulation files

---
+++ simulation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# this script will contain two simulations
+# the first simulation will be to set the environmental
+# conditions. This includes water level, quality, and soil
+# moisture. The second simulation will be the system's
+# automated reaction. This includes pulling 'sensor' data
+# and triggering reactions to data inputs.
+
+# the entire system will be looped to simulate a reaction
+# to environmental inputs:
+#for i in range(0,10):
+
+  # Environmental simulation:
+
+  # Control system simulation:
+
+
